1486e361bf06bed43fde5ba69632b2526059f5dd
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-07 18:38:49 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 // src/App.js
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AddExpense from './components/AddExpense';
 import Sidebar from './components/Sidebar';
+import { Menu } from '@material-ui/icons';
 
 const App = () => {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
     <Router>
       <div className="app">
-        <Sidebar />
+        <button className="menu-button" onClick={toggleSidebar}>
+          <Menu />
+        </button>
+        <Sidebar showSidebar={showSidebar} />
         <div className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({ showSidebar }) => {
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${showSidebar ? 'visible' : ''}`}>
       <ul>
         <li>
           <Link to="/">Home</Link>
