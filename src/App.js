13c0cb740866dde3c0f137311c538f6e762ccfbc
SVR Kalluru <raghuk.vit@gmail.com>
2024-04-07 18:35:14 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import AddExpense from './components/AddExpense';
+import Sidebar from './components/Sidebar';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add-expense" element={<AddExpense />} />
-      </Routes>
+      <div className="app">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-expense" element={<AddExpense />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+// src/components/Sidebar.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/add-expense">Add Expense</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
